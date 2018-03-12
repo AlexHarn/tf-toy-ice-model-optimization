317665bd5c0f2621060afccdf0b72df73d9657bc
Alexander Harnisch <alexanderharnisch@gmail.com>
2018-03-12 20:36:57 +0100

    Design idea should be clear now. Still the implementation is far from complete and most of it is only in my head.

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 from __future__ import division
+import tensorflow as tf
 from ice import Ice
 from detector import Detector
 from model import Model
 
 
 if __name__ == '__main__':
-    # initialize ice and detector
+    # ---------------------------- Initialization -----------------------------
     ice = Ice()
     detector = Detector()
-
-    # initialize the model
     model = Model(ice, detector)
+
+    # initialize a very weak cascade in the middle of the detector
+    model.init_cascade(500, 500, 500, n_photons=1000)
+
+    # setup TensorFLow, use GPU only for now
+    config = tf.ConfigProto(device_count={'GPU': 0})
+
+    session = tf.Session(config=config)
+    session.run(tf.global_variables_initializer())
+
+    # --------------------------------- Run -----------------------------------
+    print(session.run(model.final_positions, feed_dict={**model.feed_dict}))
--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 from __future__ import division
 import tensorflow as tf
+import numpy as np
+import settings
 
 
 class Model:
@@ -16,5 +18,53 @@ class Model:
         ice: Ice class object, which holds the scattering and absorption
              coefficients
         """
+        # set attributes
         self._ice = ice
         self._detector = detector
+
+        # define the computational graph
+        self._r0 = tf.placeholder(settings.FLOAT_PRECISION, shape=(None, 3))
+        self._v0 = tf.placeholder(settings.FLOAT_PRECISION, shape=(None, 3))
+        self.final_positions = self._r0
+
+    def init_cascade(self, x, y, z, n_photons=10000):
+        """
+        Initializes a cascade at position (x, y, z). All photons start at
+        exactly this initial position. The initial directions a sampled
+        uniformly.
+
+        Parameters
+        ----------
+        x: x coordinate of the cascade
+        y: y coordinate of the cascade
+        z: z coordinate of the cascade
+        n_photons: number of photons to initialize
+        """
+        self.r0 = np.zeros(shape=((n_photons, 3)), dtype=np.float)
+        self.v0 = np.zeros(shape=((n_photons, 3)), dtype=np.float)
+
+        self.r0[:, 0] = x
+        self.r0[:, 1] = y
+        self.r0[:, 2] = z
+
+        thetas = np.random.uniform(0, np.pi, size=n_photons)
+        phis = np.random.uniform(0, 2*np.pi, size=n_photons)
+        sinTs = np.sin(thetas)
+
+        self.v0[:, 0] = sinTs*np.cos(phis)
+        self.v0[:, 1] = sinTs*np.sin(phis)
+        self.v0[:, 2] = np.cos(thetas)
+
+    @property
+    def feed_dict(self):
+        """
+        Returns the feed_dict for the TensorFlow session.
+        """
+        if not (hasattr(self, 'r0') and hasattr(self, 'v0')):
+            raise NoPhotonsInitializedException("Photons have to be "
+                                                "initialized first!")
+        return {self._r0: self.r0, self._v0: self.v0}
+
+
+class NoPhotonsInitializedException(Exception):
+    pass
